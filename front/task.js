c4987f0dac161d7a4ae840522547b2a4c0cb8431
Marc DELOMEZ <marc.delomez@free.fr>
2019-05-04 16:20:35 +0200

    use prod env

--- front/task.js
+++ front/task.js
@@ -26,14 +26,14 @@ function source(dest, srcs, options) {
 
 
 	if (typeof options.concat == 'string') {
-		stream = stream.pipe(sourcemaps.init())
+		//stream = stream.pipe(sourcemaps.init())
 		stream = stream.pipe(concat(options.concat))
-		stream = stream.pipe(sourcemaps.write())
+		//stream = stream.pipe(sourcemaps.write())
 	}
 
-	// if (options.isCode === true) {
-	// 	stream = stream.pipe(uglify())
-	// }
+	if (options.isCode === true) {
+		stream = stream.pipe(uglify())
+	}
 
 
 	if (options.dest != undefined) {

6288c13b2ca9156aaec0cbf17d91b57e2ca06228
Marc DELOMEZ <marc.delomez@free.fr>
2020-04-17 15:12:45 +0200

    add barcode scanner app

---
+++ front/src/webapps/qrcode/src/main.js
@@ -0,0 +1,105 @@
+$$.control.registerControl('rootPage', {
+
+	template: {gulp_inject: './main.html'},
+
+
+	init: function(elt) {
+
+		const barcodeDetector = new BarcodeDetector()
+		let imageCapture = null
+		
+		function detect() {
+			imageCapture.grabFrame().then((bitmap) => {
+				return barcodeDetector.detect(bitmap)
+			})
+			.then((barcodes) => {
+				if (barcodes.length == 0) {
+					requestAnimationFrame(detect)
+				}
+				else {
+					console.log('barcodes', barcodes)
+					const {format, rawValue} = barcodes[0]
+					let content = null
+					if (format == 'qr_code') {
+						content = {
+							link: `<a href="${rawValue}" target="_blank">${rawValue}</a>`
+						}
+					}
+					else {
+						content = {
+							format,
+							value: rawValue
+						}
+					}
+					$$.ui.showAlert({title: 'BarCode Detected', content}, () => {
+						requestAnimationFrame(detect)
+					})
+				}
+			})
+			.catch((e) => {
+				console.error('BarcodeDetection failed: ', e)
+				$$.ui.showAlert({title: 'Error', content: e.message})
+			})
+		}
+
+		const ctrl = $$.viewController(elt, {
+			data: {
+				videoDevices: [],
+				showMessage: false,
+				show1: function() {
+					return this.videoDevices.length > 1
+				},
+				hasZoom: false
+			},
+			events: {
+				onCameraReady: function(ev) {
+					console.log('onCameraReady')
+					const iface = $(this).iface()
+					imageCapture = iface.getImageCapture()
+
+					setTimeout(() => {
+						const capabilities = iface.getCapabilities()
+						//console.log('capabilities', capabilities)
+
+						if (capabilities.zoom) {
+							const settings = iface.getSettings()
+							//console.log('settings', settings)
+							const {min, max, step} = capabilities.zoom
+							ctrl.scope.slider.setData({min, max, step})
+							ctrl.scope.slider.setValue(settings.zoom)
+							ctrl.setData({hasZoom: true})
+						}
+
+						requestAnimationFrame(detect)
+
+	
+					}, 500)
+					
+				},
+				onZoomChange: function(ev) {
+					const value = $(this).getValue()
+					console.log('onZoomChange', value)
+					ctrl.scope.camera.setZoom(value)
+				}
+			}
+		})
+
+		$$.util.getVideoDevices().then((videoDevices) => {
+			if (videoDevices.length > 0) {
+				ctrl.scope.camera.start()
+			}
+			else {
+				ctrl.setData({showMessage: true})
+			}
+		})
+
+		
+
+	}
+
+
+});
+
+
+
+

7fd62fe50ff2866b3832b052e605138e42eb98f2
Marc DELOMEZ <marc.delomez@free.fr>
2020-05-27 13:06:18 +0200

    refactor qrcode app

--- front/src/webapps/qrcode/src/main.js
+++ front/src/webapps/qrcode/src/main.js
@@ -1,24 +1,20 @@
 $$.control.registerControl('rootPage', {
 
-	template: {gulp_inject: './main.html'},
+	template: { gulp_inject: './main.html' },
 
 
-	init: function(elt) {
+	init: function (elt) {
 
-		const barcodeDetector = new BarcodeDetector()
-		let imageCapture = null
-		
-		function detect() {
-			imageCapture.grabFrame().then((bitmap) => {
-				return barcodeDetector.detect(bitmap)
-			})
-			.then((barcodes) => {
-				if (barcodes.length == 0) {
-					setTimeout(detect, 1000)
-				}
-				else {
-					console.log('barcodes', barcodes)
-					const {format, rawValue} = barcodes[0]
+		const ctrl = $$.viewController(elt, {
+			data: {
+				showMessage: false,
+				hasZoom: false
+			},
+			events: {
+				onBarcode: function (ev, barcode) {
+					console.log('onBarcode', barcode)
+					const iface = $(this).iface()
+					const { format, rawValue } = barcode
 					let content = null
 					if (format == 'qr_code') {
 						content = {
@@ -31,48 +27,29 @@ $$.control.registerControl('rootPage', {
 							value: rawValue
 						}
 					}
-					$$.ui.showAlert({title: 'BarCode Detected', content}, () => {
-						detect()
+					$$.ui.showAlert({ title: 'BarCode Detected', content }, () => {
+						iface.startBarcodeDetection()
 					})
-				}
-			})
-			.catch((e) => {
-				console.error('BarcodeDetection failed: ', e)
-				$$.ui.showAlert({title: 'Error', content: e.message})
-			})
-		}
-
-		const ctrl = $$.viewController(elt, {
-			data: {
-				showMessage: false,
-				hasZoom: false
-			},
-			events: {
-				onCameraReady: function(ev) {
+				},
+				onCameraReady: async function (ev) {
 					console.log('onCameraReady')
 					const iface = $(this).iface()
-					imageCapture = iface.getImageCapture()
+					const capabilities = await iface.getCapabilities()
+					console.log('capabilities', capabilities)
 
-					setTimeout(() => {
-						const capabilities = iface.getCapabilities()
-						//console.log('capabilities', capabilities)
+					if (capabilities.zoom) {
+						const settings = iface.getSettings()
+						//console.log('settings', settings)
+						const { min, max, step } = capabilities.zoom
+						ctrl.scope.slider.setData({ min, max, step })
+						ctrl.scope.slider.setValue(settings.zoom)
+						ctrl.setData({ hasZoom: true })
+					}
 
-						if (capabilities.zoom) {
-							const settings = iface.getSettings()
-							//console.log('settings', settings)
-							const {min, max, step} = capabilities.zoom
-							ctrl.scope.slider.setData({min, max, step})
-							ctrl.scope.slider.setValue(settings.zoom)
-							ctrl.setData({hasZoom: true})
-						}
+					iface.startBarcodeDetection()
 
-						detect()
-
-	
-					}, 500)
-					
 				},
-				onZoomChange: function(ev) {
+				onZoomChange: function (ev) {
 					const value = $(this).getValue()
 					console.log('onZoomChange', value)
 					ctrl.scope.camera.setZoom(value)
@@ -85,11 +62,11 @@ $$.control.registerControl('rootPage', {
 				ctrl.scope.camera.start()
 			}
 			else {
-				ctrl.setData({showMessage: true})
+				ctrl.setData({ showMessage: true })
 			}
 		})
 
-		
+
 
 	}
 

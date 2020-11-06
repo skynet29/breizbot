ebce545dbd1c16e0af94715b0740b51565775ce4
Marc DELOMEZ <marc.delomez@free.fr>
2020-11-06 18:12:54 +0100

    add label

--- front/src/webapps/money/src/pages/syntheses/syntheses.js
+++ front/src/webapps/money/src/pages/syntheses/syntheses.js
@@ -12,7 +12,7 @@ $$.control.registerControl('syntheses', {
 
         const { accountId } = this.props
 
-        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        const months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
         const ctrl = $$.viewController(elt, {
             data: {
@@ -20,16 +20,19 @@ $$.control.registerControl('syntheses', {
                 months,
                 formatIncome: function (scope) {                    
                     //const synthesis = this.syntheses[scope.idx]
-                    return  this.syntheses[scope.idx].income.toFixed(2)
+                    return (scope.idx == 0) ? 'Income' : this.syntheses[scope.idx].income.toFixed(2)
                 },
                 formatExpenses: function (scope) {
-                    return this.syntheses[scope.idx].expenses.toFixed(2)
+                    return (scope.idx == 0) ? 'Expenses' : this.syntheses[scope.idx].expenses.toFixed(2)
                 },
                 formatDifference: function (scope) {
                     const { income, expenses } = this.syntheses[scope.idx]
-                    return (income - expenses).toFixed(2)    
+                    return (scope.idx == 0) ? 'Difference' : (income - expenses).toFixed(2)    
                 },
                 getDiffColor: function(scope) {
+                    if (scope.idx == 0) {
+                        return 'black'
+                    }
                     return (parseInt(this.formatDifference(scope)) < 0) ?  'red' : 'green'
                 }
             }
@@ -40,6 +43,7 @@ $$.control.registerControl('syntheses', {
         async function load() {
             const syntheses = await http.get(`/account/${accountId}/syntheses`, { year: new Date().getFullYear() })
             //console.log('syntheses', syntheses)
+            syntheses.unshift({})
             ctrl.setData({ syntheses })
 
         }

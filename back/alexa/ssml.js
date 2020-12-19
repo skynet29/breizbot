0252b110823207e6f42816258390999e370857fa
Marc DELOMEZ <marc.delomez@free.fr>
2020-12-19 12:48:04 +0100

    fix bug

--- back/alexa/ssml.js
+++ back/alexa/ssml.js
@@ -5,27 +5,35 @@ const xmlescape = require('xml-escape')
 function sayAs(interpret, word) {
     return `<say-as interpret-as="${interpret}">${word}</say-as>`
 }
+
 function pause(duration) {
     return `<break time="${duration}" />`
 }
-function lang(lang, text) {
-    return `<lang xml:lang="${lang}">${text}</lang>`
+
+function lang(langName, text) {
+    return `<lang xml:lang="${langName}">${text}</lang>`
 }
-function say(voice, lang, text) {
-    return voice(voice, lang(lang, text))
+
+function say(voice, langName, text) {
+    return voice(voice, lang(langName, text))
 }
+
 function voice(voiceName, text) {
     return `<voice name="${voiceName}">${text}</voice>`
 }
+
 function english(text) {
     return say('Joanna', 'en-US', text)
 }
+
 function toSpeak(text) {
     return `<speak>${text}</speak>`
 }
+
 function sentence(text) {
     return `<s>${text}</s>`
 }
+
 function emphasis(text) {
     return `<emphasis level="strong">${text}</emphasis> `        
 }

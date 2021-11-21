3cb5cbb2a9a8f1d324ad832735588eb1971401ce
Marc DELOMEZ <marc.delomez@free.fr>
2021-11-21 15:21:20 +0100

    add karaoké app

---
+++ front/src/webapps/karaoke/src/main.js
@@ -0,0 +1,89 @@
+//@ts-check
+$$.control.registerControl('rootPage', {
+
+
+	template: {gulp_inject: './main.html'},
+
+	deps: ['breizbot.pager', 'breizbot.files'],
+
+	/**
+	 * 
+	 * @param {Breizbot.Services.Pager.Interface} pager 
+	 * @param {Breizbot.Services.Files.Interface} filesSrv
+	 */
+	init: function(elt, pager, filesSrv) {
+
+		/**
+		 * 
+		 * @param {string} title 
+		 * @param {string} friendUser 
+		 */
+		function openFilePage(title, friendUser) {
+			pager.pushPage('breizbot.files', {
+				title,
+				/**@type {Breizbot.Controls.Files.Props} */
+				props: {
+					filterExtension: 'mp4',
+					friendUser
+				},
+				events: {
+					/**
+					 * 
+					 * @param {Breizbot.Controls.Files.EventData.FileClick} info 
+					 */
+					fileclick: function(ev, info) {
+						console.log('fileclick', info)
+						const {rootDir, fileName } = info
+						const url = filesSrv.fileUrl(rootDir + fileName, friendUser)
+						pager.pushPage('VidoCtrl', {
+							title: 'Video',
+							props: {
+								url
+							}
+						})
+	
+					}
+				}	
+			})				
+
+		}
+
+		const ctrl = $$.viewController(elt, {
+			data: {
+			},
+			events: {
+				onHome: function() {
+					openFilePage('Home files', '')
+				},
+				onShare: function() {
+					pager.pushPage('breizbot.friends', {
+						title: 'Shared files',
+						/**@type {Breizbot.Controls.Friends.Props} */
+						props: {
+							showConnectionState: false
+						},
+						events: {
+							/**
+							 * 
+							 * @param {Breizbot.Controls.Friends.EventData.FriendClick} data 
+							 */
+							friendclick: function(ev, data) {
+								//console.log('onSelectFriend', data)
+								const {userName} = data
+								openFilePage(userName, userName)			
+							}
+						}					
+					})
+				}
+
+			}
+		})
+
+	}
+
+
+});
+
+
+
+
---
+++ front/src/webapps/karaoke/src/video.js
@@ -0,0 +1,149 @@
+//@ts-check
+$$.control.registerControl('VidoCtrl', {
+
+	template: { gulp_inject: './video.html' },
+
+	deps: ['breizbot.files'],
+
+	props: {
+		url: '#'
+	},
+
+	/**
+	 * 
+	 * @param {Breizbot.Services.Files.Interface} files 
+	 */
+	init: function (elt, files) {
+
+		const {url} = this.props
+
+		const ctrl = $$.viewController(elt, {
+			data: {
+				micGain: 0.5,
+				videoGain: 0.5,
+				url
+			},
+			events: {
+				onMicGainChange: function(ev, data) {
+					console.log('onMicGainChange', data)
+					micGainNode.gain.value = data
+				},
+				onVideoGainChange: function(ev, data) {
+					console.log('onVideoGainChange', data)
+					videoElt.volume = data
+				}
+			}
+		})
+
+		let stream = null
+
+		const canvas = ctrl.scope.canvas.get(0)
+		const canvasCtx = canvas.getContext('2d')
+		const audioCtx = new AudioContext()
+		const micGainNode = audioCtx.createGain()
+		micGainNode.gain.value = ctrl.model.micGain
+		/**@type {HTMLVideoElement} */
+		const videoElt = ctrl.scope.video.get(0)
+		ctrl.setData({videoGain: videoElt.volume})
+		const videoSource = audioCtx.createMediaElementSource(videoElt)
+
+		function visualize(source) {
+			const analyser = audioCtx.createAnalyser()
+
+			analyser.fftSize = 2048
+
+			const bufferLength = analyser.frequencyBinCount
+			const dataArray = new Uint8Array(bufferLength)
+
+			source.connect(analyser)
+
+			draw()
+
+			function draw() {
+				const width = canvas.width
+				const height = canvas.height
+
+				requestAnimationFrame(draw)
+
+				analyser.getByteTimeDomainData(dataArray)
+
+				canvasCtx.fillStyle = 'rgb(200, 200, 200)'
+				canvasCtx.fillRect(0, 0, width, height)
+
+				canvasCtx.lineWidth = 2
+				canvasCtx.strokeStyle = 'rgb(0, 0, 0)'
+
+				canvasCtx.beginPath()
+
+				const sliceWidth = width / bufferLength
+				let x = 0
+
+				for (let i = 0; i < bufferLength; i++) {
+					const v = dataArray[i] / 128.0
+					const y = v * height / 2
+
+					if (i == 0) {
+						canvasCtx.moveTo(x, y)
+					}
+					else {
+						canvasCtx.lineTo(x, y)
+					}
+
+					x += sliceWidth
+
+				}
+
+				canvasCtx.lineTo(width, height / 2)
+				canvasCtx.stroke()
+			}
+
+		}
+
+		async function init() {
+			//try {
+				stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+				const source = audioCtx.createMediaStreamSource(stream)
+				source.connect(micGainNode)
+
+				const splitter = audioCtx.createChannelSplitter()
+				videoSource.connect(splitter)
+
+				const merger = audioCtx.createChannelMerger()
+				micGainNode.connect(merger, 0, 0)
+				micGainNode.connect(merger, 0, 1)
+				splitter.connect(merger, 0, 0)
+				splitter.connect(merger, 1, 1)
+
+
+				merger.connect(audioCtx.destination)
+
+				visualize(source)	
+			// }	
+			// catch(e)	
+			// {
+			// 	console.error(e)
+			// }
+		}
+		
+		this.dispose = function() {
+			console.log('dispose')
+			if (stream) {
+				stream.getTracks().forEach(function (track) {
+					track.stop();
+				})
+				stream = null
+			}			
+		}
+		
+
+
+		init()
+	}
+
+
+
+});
+
+
+
+

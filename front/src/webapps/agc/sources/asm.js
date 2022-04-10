b4c3d8a4def79d0ab32b4c3e90cb6f745642bd07
Marc DELOMEZ <marc.delomez@free.fr>
2022-04-10 14:18:00 +0200

    refactor asm tool

--- front/src/webapps/agc/sources/asm.js
+++ front/src/webapps/agc/sources/asm.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
+const { off } = require('process')
 
 const insts = {
-    TC: { opcode: 0, info: ['Q <- Z | Z <- k'] },
+    TC: { opcode: 0, info: ['Q <- Z | Z <- k'], label: 'Transfer Control' },
     CSS: {
         opcode: 1, qcode: 0,
         info: [
@@ -10,66 +11,70 @@ const insts = {
             'if M[k] =+0: Z <- Z+1',
             'if M[k] <-0: Z <- Z+2',
             'if M[k] =-0: Z <- Z+3'
-        ]
+        ],
+        label: 'Count, Compare and Skip'
     },
-    TCF: { opcode: 1, info: 'Z <- k' },
+    TCF: { opcode: 1, info: 'Z <- k', label: 'Transfer Control to Fixed' },
 
-    DAS: { opcode: 2, qcode: 0 },
-    LXCH: { opcode: 2, qcode: 1 },
-    INCR: { opcode: 2, qcode: 2, info: 'M[k] <- M[k] + 1' },
-    ADS: { opcode: 2, qcode: 3 },
+    DAS: { opcode: 2, qcode: 0, info: 'M[k, k+] <- A,L + M[k, k+1]', label: 'Double Add to Storage' },
+    LXCH: { opcode: 2, qcode: 1, label: 'Exchange L and K' },
+    INCR: { opcode: 2, qcode: 2, info: 'M[k] <- M[k] + 1', label: 'Increment' },
+    ADS: { opcode: 2, qcode: 3, info: ['A <- A + M[k]', 'M[k] <- A'], label: 'Add To Storage' },
 
-    CA: { opcode: 3, info: 'A <- M[k]' },
-    CS: { opcode: 4, info: 'A <- -M[k]' },
+    CA: { opcode: 3, info: 'A <- M[k]', label: 'Clear and Add' },
+    CS: { opcode: 4, info: 'A <- -M[k]', label: 'Clear and Subtract' },
 
     INDEX: { opcode: 5, qcode: 0 },
-    DXCH: { opcode: 5, qcode: 1, info: ['A <-> M[k]', 'L <-> M[k+1]'] },
+    DXCH: { opcode: 5, qcode: 1, info: ['A <-> M[k]', 'L <-> M[k+1]'], label: 'Double Exchange' },
     TS: { opcode: 5, qcode: 2, info: 'M[k] <- A' },
-    XCH: { opcode: 5, qcode: 3, info: 'A <-> M[k]' },
+    XCH: { opcode: 5, qcode: 3, info: 'A <-> M[k]', label: 'Exchange A and K' },
 
-    AD: { opcode: 6, info: 'A <- A + M[k]' },
-    MASK: { opcode: 7 },
+    AD: { opcode: 6, info: 'A <- A + M[k]', label: 'Add' },
+    MASK: { opcode: 7, label: 'Mask A by K' },
 
-    READ: { opcode: 0o10, iocode: 0, info: 'A <- IO[kc]' },
-    WRITE: { opcode: 0o10, iocode: 1, info: 'IO[kc] <- A' },
+    READ: { opcode: 0o10, iocode: 0, info: 'A <- IO[kc]', label: 'Read Channel KC' },
+    WRITE: { opcode: 0o10, iocode: 1, info: 'IO[kc] <- A', label: 'Write Channel KC' },
     RAND: { opcode: 0o10, iocode: 2, info: 'A <- A & IO[kc]' },
-    WAND: { opcode: 0o10, iocode: 3, info: 'IO[kc] <- A & IO[kc]' },
+    WAND: { opcode: 0o10, iocode: 3, info: 'IO[kc] <- A & IO[kc]', label: 'Write and Mask' },
     ROR: { opcode: 0o10, iocode: 4, info: 'A <- A | IO[kc]' },
-    WOR: { opcode: 0o10, iocode: 5, info: 'IO[kc] <- A | IO[kc]' },
+    WOR: { opcode: 0o10, iocode: 5, info: 'IO[kc] <- A | IO[kc]', label: 'Write and Superimpose' },
     RXOR: { opcode: 0o10, iocode: 6, info: 'A <- A ^ IO[kc]' },
-    EDRUPT: { opcode: 0o10, iocode: 7, info: 'A <- A ^ IO[kc]' },
+    EDRUPT: { opcode: 0o10, iocode: 7, info: 'For machine checkout only' },
 
     DV: {
         opcode: 0o11, qcode: 0, info: [
             'A <- A,B / M[k]',
             'B <- A,B % M[k]'
-        ]
+        ],
+        label: 'Divide'
     },
-    BZF: { opcode: 0o11, info: 'if A = +/-0: Z <- k' },
+    BZF: { opcode: 0o11, info: 'if A = +/-0: Z <- k', label: 'Branch Zero to Fixed' },
 
-    MSU: { opcode: 0o12, qcode: 0 },
-    QXCH: { opcode: 0o12, qcode: 1 },
+    MSU: { opcode: 0o12, qcode: 0, label: 'Modular Subtract' },
+    QXCH: { opcode: 0o12, qcode: 1, label: 'Exchange Q and K' },
     AUG: {
         opcode: 0o12, qcode: 2, info: [
             'if M[k] >= +0: M[k] <- M[k] + 1',
             'if M[k] <= -0: M[k] <- M[k] - 1'
-        ]
+        ],
+        label: 'Augment'
     },
     DIM: {
         opcode: 0o12, qcode: 3, info: [
             'if M[k] > +0: M[k] <- M[k] - 1',
             'if M[k] < -0: M[k] <- M[k] + 1'
-        ]
+        ],
+        label: 'Diminish'
     },
 
-    DCA: { opcode: 0o13, info: 'A,L <- M[k, k+1]' },
-    DCS: { opcode: 0o14, info: 'A,L <- -M[k, k+1]' },
+    DCA: { opcode: 0o13, info: 'A,L <- M[k, k+1]', label: 'Double Clear and Add' },
+    DCS: { opcode: 0o14, info: ['A <- -M[k]', 'L <- -M[k+1]'], label: 'Double Clear abd Subtract' },
     NDX: { opcode: 0o15 },
 
-    BZMF: { opcode: 0o16, info: 'if A <= +/-0: Z <- k' },
-    SU: { opcode: 0o16, qcode: 0, info: 'A <- A - M[k]' },
+    BZMF: { opcode: 0o16, info: 'if A <= +/-0: Z <- k', label: 'Branch ZEro or Minus to Fixed' },
+    SU: { opcode: 0o16, qcode: 0, info: 'A <- A - M[k]', label: 'Subtract' },
 
-    MP: { opcode: 0o17, info: 'A,L <- A x M[k]' },
+    MP: { opcode: 0o17, info: 'A,L <- A x M[k]', label: 'Multiply' },
 }
 
 const registers = {
@@ -91,43 +96,35 @@ const registers = {
 }
 
 const alias = {
-    //2: 'RETURN',
-    4: 'INHINT',
-    3: 'RELINT',
-    0x17: 'RESUME',
+    0o00002: { name: 'RETURN', label: 'Return from Subroutine' },
+    0o50017: { name: 'RESUME', label: 'Resume Interrupted Program' },
+    0o00003: { name: 'RELINT', label: 'Enable Interrupts' },
+    0o00004: { name: 'INHINT', label: 'Disable Interrupts' },
+    0o20001: { name: 'DDOUBL', label: 'Double Precision Double' },
+    0o40001: { name: 'DCOM', label: 'Double Complement' },
+    0o40000: { name: 'COM', label: 'Complement the Contents of A' },
+    0o60000: { name: 'DOUBLE', label: 'Double the Contents of A' },
+    0o52006: { name: 'DTCB', label: 'Double Transfer Control, Switching Both Banks' },
+    0o52005: { name: 'DTCF', label: 'Double Transfer Control, Switching F Bank' },
+    0o54000: { name: 'OVSK', label: 'Overflow Skip' },
+    0o70000: { name: 'SQUARE', label: 'Square the Contents of A' },
+    0o54005: { name: 'TCAA', label: 'Transfer Control to Address in A' },
+    0o00001: { name: 'XLQ', label: 'Execute Using L and Q' }
 }
+const alias2 = {}
+for (const [key, { name, label }] of Object.entries(alias)) {
+    alias2[name] = { code: parseInt(key), label }
+}
+
 const arg2 = process.argv[2]
 
 if (arg2 == '-i') {
     const arg = process.argv[3]
-    if (arg == undefined) {
-        for (const i of Object.keys(insts).sort()) {
-            console.log(i)
-        }
-        process.exit(0)
-    }
-    if (insts[arg] == undefined) {
-        console.log('Unknown instruction')
-        process.exit(0)
-    }
-    let list = insts[arg].info
-    if (list == undefined) {
-        console.log('No info available !')
-    }
-    else {
-        if (typeof list == 'string') {
-            list = [list]
-        }
-        for (const i of list) {
-            console.log(i)
-        }
+    showInfo(arg)
 
-    }
-    process.exit(0)
 }
 else if (arg2 == '-f') {
     const fileName = process.argv[3]
-    const fd = fs.openSync(fileName, 'r')
     let length = 10
     let offset = 0
     if (process.argv[4] == '-n' && process.argv[5] != undefined) {
@@ -136,7 +133,66 @@ else if (arg2 == '-f') {
     if (process.argv[4] == '-s' && process.argv[5] != undefined) {
         offset = parseInt(process.argv[5])
     }
+
     console.log({ length, offset })
+    decodeFile(fileName, offset, length)
+}
+else {
+    const inputLine = process.argv[2]
+    const pgm = inputLine.split(',').map((v) => parseInt(v, 8))
+    //console.log(pgm)
+    decode(pgm)    
+}
+
+
+function format(a, addr) {
+    let ret = ''
+    if (addr != undefined) {
+        ret += addr.toString(8).padStart(4, '0') + '   '
+    }
+    return ret + a.toString(8).padStart(5, '0')
+}
+
+function showInfo(arg) {
+    if (arg == undefined) {
+        for (const i of Object.keys(insts).sort()) {
+            console.log(i)
+        }
+        console.log('\nAlias:\n')
+        for (const i of Object.keys(alias2).sort()) {
+            console.log(i)
+        }
+    }
+    else if (alias2[arg] != undefined) {
+        const { label, code } = alias2[arg]
+        console.log('Alias')
+        const { addr, key } = decodeSingleInst(code, false)
+        console.log(format(code), key, addr)
+        console.log(label)
+    }
+    else if (insts[arg] == undefined) {
+        console.log('Unknown instruction')
+    }
+    else {
+        let { info: list, label, opcode } = insts[arg]
+        if (label != undefined) {
+            console.log(label)
+        }
+        console.log('extend', (opcode & 0o10) != 0)
+        if (list != undefined) {
+            if (typeof list == 'string') {
+                list = [list]
+            }
+            for (const i of list) {
+                console.log(i)
+            }
+        }
+    }
+
+}
+
+function decodeFile(fileName, offset, length) {
+    const fd = fs.openSync(fileName, 'r')
     const buffer = Buffer.alloc(length)
     const nbByteRead = fs.readSync(fd, buffer, 0, length, offset)
     console.log({ nbByteRead })
@@ -147,23 +203,49 @@ else if (arg2 == '-f') {
         pgm.push(view.getUint16(i, false) >> 1) // drop parity bit
     }
     decode(pgm, 0o4000 + offset)
-
-    process.exit(0)
 }
 
-const inputLine = process.argv[2]
-const pgm = inputLine.split(',').map((v) => parseInt(v, 8))
-console.log(pgm)
-decode(pgm)
+function decodeSingleInst(inst, extend) {
+    const opcode = (inst >> 12) & 0b111
+    const qcode = (inst >> 10) & 0b11
+    const iocode = (inst >> 9) & 0b111
+    let excode = opcode
 
-function format(a, addr) {
-    let ret = ''
-    if (addr != undefined) {
-        ret += addr.toString(8).padStart(4, '0')
+    if (extend) {
+        excode |= 0o10
     }
-    return ret + '   ' + a.toString(8).padStart(5, '0')
-}
 
+    //console.log({opcode, qcode, extend})
+    for (const [key, data] of Object.entries(insts)) {
+        if (data.qcode != undefined) {
+            if (data.qcode != qcode)
+                continue
+        }
+        if (data.iocode != undefined) {
+            if (data.iocode != iocode)
+                continue
+        }
+
+        if (excode == data.opcode) {
+            let addr = inst - (opcode << 12)
+            if (data.qcode != undefined) {
+                addr -= (qcode << 10)
+            }
+            if (data.iocode != undefined) {
+                addr -= (iocode << 9)
+            }
+
+            if (['DXCH', 'DCA', 'DCS'].includes(key)) {
+                addr--;
+            }
+
+            const reg = registers[addr]
+            addr = (reg != undefined) ? reg : addr.toString(8)
+            return { key, addr }
+        }
+    }
+
+}
 
 function decode(pgm, startAddr) {
 
@@ -172,56 +254,17 @@ function decode(pgm, startAddr) {
         if (inst == 6) {
             extend = true
             console.log(format(inst, startAddr), 'EXTEND')
-            startAddr++
-            continue
         }
-
-        if (alias[inst]) {
-            console.log(format(inst, startAddr), alias[inst])
-            startAddr++
-            continue
+        else if (alias[inst] != undefined) {
+            console.log(format(inst, startAddr), alias[inst].name)
         }
-
-        const opcode = (inst >> 12) & 0b111
-        const qcode = (inst >> 10) & 0b11
-        const iocode = (inst >> 9) & 0b111
-        let excode = opcode
-
-        if (extend) {
-            excode |= 0o10
+        else {
+            const { key, addr } = decodeSingleInst(inst, extend)
             extend = false
+            console.log(format(inst, startAddr), key.padEnd(6, ' '), addr)
         }
 
-        //console.log({opcode, qcode, extend})
-        for (const [key, data] of Object.entries(insts)) {
-            if (data.qcode != undefined) {
-                if (data.qcode != qcode)
-                    continue
-            }
-            if (data.iocode != undefined) {
-                if (data.iocode != iocode)
-                    continue
-            }
-
-            if (excode == data.opcode) {
-                let addr = inst - (opcode << 12)
-                if (data.qcode != undefined) {
-                    addr -= (qcode << 10)
-                }
-                if (data.iocode != undefined) {
-                    addr -= (iocode << 9)
-                }
-
-                if (key == 'DXCH') {
-                    addr--;
-                }
-
-                const reg = registers[addr]
-                addr = (reg != undefined) ? reg : addr.toString(8)
-                console.log(format(inst, startAddr), key.padEnd(6, ' '), addr)
-            }
-        }
-
-        startAddr++;
+        if (startAddr != undefined)
+            startAddr++
     }
 }

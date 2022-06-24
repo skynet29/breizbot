8db783163817e031f09316f27128b1fe5a7e2ef9
Marc DELOMEZ <marc.delomez@free.fr>
2022-06-24 16:24:50 +0200

    rename app; modify karapke to video cast

--- front/src/webapps/videocast/src/main.js
+++ front/src/webapps/videocast/src/main.js
@@ -39,20 +39,14 @@ $$.control.registerControl('rootPage', {
 
 		const ctrl = $$.viewController(elt, {
 			data: {
-				micGain: 0.5,
 				videoGain: 0.5,
 				url: '#',
-				audioDevices: [],
-				showAnalyser: false,
-				status: 'KO',
-				recording: false,
 				showStartRecord: function () {
 					return this.status == 'OK' && !this.recording
 				},
 				isDisplayAvailable: false,
 				isDisplayStarted: false,
 				isPlaying: false,
-				karaokeEnabled: false,
 				duration: 0,
 				curTime: 0,	
 				getDuration: function() {
@@ -66,10 +60,6 @@ $$.control.registerControl('rootPage', {
 				},
 				onSyncDisplayWithMain: function() {
 					display.setCurrentTime(videoElt.currentTime)
-				},
-				onKaraokeChange: function(evt, data) {
-					console.log('onKaraokeChange', data)
-					display.enableKaraoke(data)
 				},
 				onPlay: function() {
 					display.play()
@@ -89,10 +79,6 @@ $$.control.registerControl('rootPage', {
 					display.setUrl(ctrl.model.url)
 					ctrl.setData({duration: videoElt.duration, isPlaying: false})
 				},
-				onMicGainChange: function (ev, data) {
-					//console.log('onMicGainChange', data)
-					micGainNode.gain.value = data
-				},
 				onVideoGainChange: function (ev, data) {
 					//console.log('onVideoGainChange', data)
 					if (display.isStarted()) {
@@ -101,11 +87,6 @@ $$.control.registerControl('rootPage', {
 					else {
 						videoElt.volume = data
 					}
-				},
-				onAudioDeviceChange: async function () {
-					const deviceId = $(this).getValue()
-					//console.log('onAudioDeviceChange', deviceId)
-					await initNodes(buildContraints(deviceId))
 				},
 				onChooseFile: function () {
 					pager.pushPage('breizbot.filechooser', {
@@ -148,58 +129,11 @@ $$.control.registerControl('rootPage', {
 			ctrl.setData({curTime: value})
 		})
 
-		function buildContraints(deviceId) {
-			return {
-				audio: {
-					deviceId: {
-						exact: deviceId
-					}
-				}
-			}
-		}
-
 
 		/**@type {HTMLVideoElement} */
 		const videoElt = ctrl.scope.video.get(0)
 
-		const audioCtx = new AudioContext()
 
-		const micGainNode = audioCtx.createGain()
-		micGainNode.gain.value = ctrl.model.micGain
-
-		async function getAudioInputDevices() {
-			const audioDevices = await $$.media.getAudioInputDevices()
-			ctrl.setData({
-				audioDevices: audioDevices.map((i) => {
-					return { value: i.id, label: i.label }
-				})
-			})
-		}
-
-		async function initNodes(constraints) {
-			console.log('initNodes', constraints)
-			try {
-				stream = await navigator.mediaDevices.getUserMedia(constraints)
-				const audioSource = audioCtx.createMediaStreamSource(stream)
-				const dest = audioCtx.createMediaStreamDestination()
-
-				audioSource.connect(micGainNode)
-				micGainNode.connect(audioCtx.destination)
-				ctrl.setData({ status: 'OK' })
-
-			}
-			catch (e) {
-				ctrl.setData({ status: 'KO' })
-				console.error(e)
-			}
-		}
-
-		async function init() {
-			await getAudioInputDevices()
-			if (ctrl.model.audioDevices.length > 0) {
-				await initNodes(buildContraints(ctrl.model.audioDevices[0].value))
-			}
-		}
 
 		this.dispose = function () {
 			console.log('dispose')
@@ -213,7 +147,6 @@ $$.control.registerControl('rootPage', {
 
 
 
-		init()
 	}
 
 
